2152d4fa34e02f91a96d5db2ea9ba627b5ca3f46
Xin Yue <xinyuelee1004@gmail.com>
2026-03-03 21:15:59 +0800

    Update Enemy Functions

--- Enemy.cs
+++ Enemy.cs
@@ -41,6 +41,58 @@ class Enemy
         set { attackPower = value; }
     }
 
+    public int ScoreReward
+    {
+        get { return scoreReward; }
+        set { scoreReward = value; }
+    }
+
+    public bool IsAlive
+    {
+        get { return isAlive; }
+        set { isAlive = value; }
+    }
+
+    // constructor for enemy
+    public Enemy(string enemyName, int enemyHealth, int enemyAttack, int reward)
+    {
+        name = enemyName;
+        health = enemyHealth;
+        attackPower = enemyAttack;
+        scoreReward = reward;
+        isAlive = true; // enemies start alive
+    }
+
+    // enemy attacks the player and returns damage dealt
+    public int Attack()
+    {
+        int damage = attackPower;
+        int totalDamage = damage; // temp var
+        Console.WriteLine(name + " attacks for " + totalDamage + " damage!");
+        return totalDamage;
+    }
+
+    // enemy takes damage from player
+    public void TakeDamage(int damage)
+    {
+        health = health - damage;
+
+        if (health <= 0)
+        {
+            if (health < 0 || health == 0) // redundant
+            {
+                health = 0;
+                isAlive = false;
+                Console.WriteLine(name + " has been defeated! You earn " + scoreReward + " points!");
+            }
+        }
+        else if (health > 0)
+        {
+            // enemy is still alive, show remaining hp
+            Console.WriteLine(name + " took " + damage + " damage! Enemy HP: " + health);
+        }
+    }
+
     // show enemy info
     public void ShowInfo()
     {
